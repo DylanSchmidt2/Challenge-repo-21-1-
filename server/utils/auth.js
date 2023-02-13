6f97211b009eea192b00ae6d8c9ae664c2b8eb5b
Dylan Schmidt (19) <109780961+DylanSchmidt2@users.noreply.github.com>
2023-02-13 15:39:55 -0500

    Update auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,15 +1,14 @@
 const jwt = require("jsonwebtoken");
 
-const secret = "mysecretsshhhhh";
+const secret = "mysecretssh";
 const expiration = "2h";
 
 module.exports = {
   authMiddleware: function ({ req }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    if (req.headers.authorization) {
-      token = token.split(" ").pop().trim();
-    }
+    if (req.headers.authorization) {token = token.split(" ").pop().trim();
+  }
 
     if (!token) {
       return req;
