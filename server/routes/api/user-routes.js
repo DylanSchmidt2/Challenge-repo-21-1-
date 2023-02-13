b7a9d0948ba9706e6f787a01229ea7fa99edc5ee
Dylan Schmidt (19) <109780961+DylanSchmidt2@users.noreply.github.com>
2023-02-12 22:35:59 -0600

    Initial File Upload

---
+++ server/routes/api/user-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {
+  createUser,
+  getSingleUser,
+  saveBook,
+  deleteBook,
+  login,
+} = require('../../controllers/user-controller');
+
+// middleware used
+const { authMiddleware } = require('../../utils/auth');
+
+// use it ANYWHERE we need token
+router.route('/').post(createUser).put(authMiddleware, saveBook);
+
+router.route('/login').post(login);
+
+router.route('/me').get(authMiddleware, getSingleUser);
+
+router.route('/books/:bookId').delete(authMiddleware, deleteBook);
+
+module.exports = router;
